a181865b63d3db0e2a768ea3fc4072ef75fdab96
BeetleFunk <124721033+BeetleFunk@users.noreply.github.com>
2024-03-08 12:27:34 -0500

    Move common code into setup runner for GC tests

--- tests/gc_tests.rs
+++ tests/gc_tests.rs
@@ -4,13 +4,58 @@ use siskin::error::BasicResult;
 
 type TestResult = BasicResult<()>;
 
-// run siskin code using a fresh interpreter environment and return a string containing the program output
+// write the definition once since these classes are used in multiple garbage collection tests
+const LINKED_LIST_DEFINITION: &str = "\
+    class Node {
+        init(value) {
+            this.value = value;
+        }
+    }
+
+    class LinkedList {
+        init() {
+            this.start = nil;
+            this.end = nil;
+        }
+
+        push(value) {
+            var node = Node(value);
+            if (this.start == nil) {
+                node.prev = nil;
+                node.next = nil;
+                this.start = node;
+                this.end = node;
+            } else {
+                node.prev = this.end;
+                node.next = nil;
+                this.end.next = node;
+                this.end = node;
+            }
+        }
+
+        toString() {
+            if (this.start == nil) {
+                return \"EMPTY LIST\";
+            }
+
+            var current = this.start;
+            var contents = \"[\" + toString(current.value);
+            while (current.next != nil) {
+                current = current.next;
+                contents = contents + \", \" + toString(current.value);
+            }
+            contents = contents + \"]\";
+            return contents;
+        }
+    }";
+
+// setup common code in a new Siskin VM, run the provided test script, and return a string containing the program output
 fn run(code: &str) -> error::BasicResult<String> {
     let mut buffer = Vec::new();
-    bytecode::interpret_ephemeral(code, &mut buffer)?;
-
+    let mut vm_state = bytecode::create_vm();
+    bytecode::interpret(&mut vm_state, LINKED_LIST_DEFINITION, &mut buffer)?;
+    bytecode::interpret(&mut vm_state, code, &mut buffer)?;
     let output = std::str::from_utf8(buffer.as_slice()).unwrap();
-
     Ok(output.to_string())
 }
 
@@ -18,74 +63,32 @@ fn run(code: &str) -> error::BasicResult<String> {
 // the original strategy of primitive reference counting (using Rust's Rc) did not reclaim circular references once they went out of scope
 fn circular_references() -> TestResult {
     let code = "\
-        class Node {
-            init(value) {
-                this.value = value;
-            }
-        }
-
-        class List {
-            init() {
-                this.start = nil;
-                this.end = nil;
-            }
-
-            push(value) {
-                var node = Node(value);
-                if (this.start == nil) {
-                    node.prev = nil;
-                    node.next = nil;
-                    this.start = node;
-                    this.end = node;
-                } else {
-                    node.prev = this.end;
-                    node.next = nil;
-                    this.end.next = node;
-                    this.end = node;
-                }
-            }
-
-            toString() {
-                if (this.start == nil) {
-                    return \"EMPTY LIST\";
-                }
-
-                var current = this.start;
-                var contents = \"[\" + toString(current.value);
-                while (current.next != nil) {
-                    current = current.next;
-                    contents = contents + \", \" + toString(current.value);
-                }
-                contents = contents + \"]\";
-                return contents;
-            }
-        }
-
+        var heapSizeBeforeGC;
         {
-            var outerList = List();
+            var outerList = LinkedList();
             var numLists = 100;
-            var innerListSize = 100;
+            var innerListSize = 200;
             for (var i = 0; i < numLists; i = i + 1) {
-                var innerList = List();
+                var innerList = LinkedList();
                 for (var j = 0; j < innerListSize; j = j + 1) {
                     innerList.push(j);
                 }
                 outerList.push(innerList);
             }
             forceGC();
-            print \"Made a big list!\";
-            print \"heap size: \" + getNumHeapEntries();
+            print \"Made a big list on the heap.\";
+            heapSizeBeforeGC = getNumHeapEntries();
         }
+        print \"List is now out of scope. Forcing garbage collection.\";
         forceGC();
-        print \"List is now out of scope!\";
-        print \"heap size: \" + getNumHeapEntries();";
+        var heapEntriesFreed = heapSizeBeforeGC - getNumHeapEntries();
+        print \"Freed \" + heapEntriesFreed + \" heap entries\";";
 
     let output = run(code)?;
     let expected = "\
-        Made a big list!\n\
-        heap size: 10213\n\
-        List is now out of scope!\n\
-        heap size: 12\n";
+        Made a big list on the heap.\n\
+        List is now out of scope. Forcing garbage collection.\n\
+        Freed 20201 heap entries\n";
     assert_eq!(expected, output);
     Ok(())
 }
@@ -93,83 +96,41 @@ fn circular_references() -> TestResult {
 #[test]
 fn relocated_heap_references() -> TestResult {
     let code = "\
-        class Node {
-            init(value) {
-                this.value = value;
-                this.prev = nil;
-                this.next = nil;
-            }
-        }
-
-        class List {
-            init() {
-                this.start = nil;
-                this.end = nil;
-            }
-
-            push(value) {
-                var node = Node(value);
-                if (this.start == nil) {
-                    node.prev = nil;
-                    node.next = nil;
-                    this.start = node;
-                    this.end = node;
-                } else {
-                    node.prev = this.end;
-                    node.next = nil;
-                    this.end.next = node;
-                    this.end = node;
-                }
-            }
-
-            toString() {
-                if (this.start == nil) {
-                    return \"EMPTY LIST\";
-                }
-
-                var current = this.start;
-                var contents = \"[\" + toString(current.value);
-                while (current.next != nil) {
-                    current = current.next;
-                    contents = contents + \", \" + toString(current.value);
-                }
-                contents = contents + \"]\";
-                return contents;
-            }
-        }
         var global1 = Node(1);
         var global2 = global1;
         var global3;
+        var heapSizeBeforeGC;
         {
-            var outerList = List();
+            var outerList = LinkedList();
             var numLists = 100;
             var innerListSize = 100;
             for (var i = 0; i < numLists; i = i + 1) {
-                var innerList = List();
+                var innerList = LinkedList();
                 for (var j = 0; j < innerListSize; j = j + 1) {
                     innerList.push(j);
                 }
                 outerList.push(innerList);
             }
-            forceGC();
-            print \"Made a big list!\";
-            print \"heap size: \" + getNumHeapEntries();
+            // set these globals to new heap objects that should exist after the big list
             global2 = Node(3);
             global3 = Node(5);
+            forceGC();
+            print \"Made a big list on the heap.\";
+            heapSizeBeforeGC = getNumHeapEntries();
         }
+        print \"List is now out of scope. Forcing garbage collection.\";
         forceGC();
-        print \"List is now out of scope!\";
-        print \"heap size: \" + getNumHeapEntries();
+        var heapEntriesFreed = heapSizeBeforeGC - getNumHeapEntries();
+        print \"Freed \" + heapEntriesFreed + \" heap entries\";
         print \"Read global1.value: \" + toString(global1.value);
         print \"Read global2.value: \" + toString(global2.value);
         print \"Read global3.value: \" + toString(global3.value);";
 
     let output = run(code)?;
     let expected = "\
-        Made a big list!\n\
-        heap size: 10214\n\
-        List is now out of scope!\n\
-        heap size: 15\n\
+        Made a big list on the heap.\n\
+        List is now out of scope. Forcing garbage collection.\n\
+        Freed 10201 heap entries\n\
         Read global1.value: 1\n\
         Read global2.value: 3\n\
         Read global3.value: 5\n";
